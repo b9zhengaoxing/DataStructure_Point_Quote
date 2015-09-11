bc86aa644c151d66d6be66ce84d7819e3e8946c1
b9zhengaoxing <1164834201@qq.com>
2015-09-11 10:51:07 +0800

    Initial Commit

---
+++ DataStructure_Point_Quote/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructure_Point_Quote
+//
+//  Created by tengfei li on 11/9/15.
+//  Copyright (c) 2015年 LYD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
